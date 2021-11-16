536f926f81ff970ea17675960439f6ff3032dc4f
calum-atkins <calum2atkins@gmail.com>
2021-11-16 18:15:55 +0000

    Added candles and market class with some attributes

---
+++ src/BackEnd/Markets/Candles.java
@@ -0,0 +1,9 @@
+package BackEnd.Markets;
+
+public class Candles extends Market {
+    private int open;
+    private int close;
+    private int low;
+    private int high;
+
+}
---
+++ src/BackEnd/Markets/Market.java
@@ -0,0 +1,11 @@
+package BackEnd.Markets;
+
+public class Market {
+    private String index;
+    private Candles[] oneHourCandles;
+    private Candles[] fourHourCandles;
+    private Candles[] dayCandles;
+
+    //
+
+}
